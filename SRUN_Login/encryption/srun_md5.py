c43802f976e3b3c107521d18c8330881a9daf094
Anchor-GitHu <593514148@qq.com>
2026-09-03 00:49:58 +0800

    移除 SRUN_Login 目录，加入 .gitignore

--- SRUN_Login/encryption/srun_md5.py
+++
@@ -1,6 +0,0 @@
-import hmac
-import hashlib
-
-
-def get_md5(password, token):
-    return hmac.new(token.encode(), password.encode(), hashlib.md5).hexdigest()
--- SRUN_Login/encryption/srun_sha1.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-
-def get_sha1(value):
-    return hashlib.sha1(value.encode()).hexdigest()
